88b31dfb2c79018cfd866f94a895b0812b47927d
dfelig <felig@mail.tau.ac.il>
2021-09-09 20:50:26 +0300

    updated python

--- spkmeans.py
+++ spkmeans.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import mykmeanssp as spk
 
+
 def main(original_k: int, goal: str, file_name: str):
     data = pd.read_csv(file_name, header=None)
     data_arr = data.values.tolist()
@@ -10,27 +11,38 @@ def main(original_k: int, goal: str, file_name: str):
 
     if goal == 'spk':
         observations = spk.c_code(original_k, data_arr, 0, original_n, original_dim)  # should be a 2d array
-        print("back to python")
         n = len(observations)
         dim = len(observations[0])
         k = len(observations[0])
 
-        distances = np.full(n, np.inf)  # distances[i] = distance from observations[i] to closest centroid
-        centroids = np.full(k, -1)  # 1d array of size k, holds indices of centroids
-
-        np.random.seed(0)
-        centroid_index = np.random.choice(n)
         z = 0
-        while z < k:
-            centroids[z] = centroid_index
-            distances = update_distances(distances, centroids, observations, z, n)
-            centroid_index = choose_next_centroid(distances, observations)
-            z += 1
-        initial_centroids = centroids.astype(int).tolist()
-        original_indices = [int(initial_centroids[i]) for i in range(k)]  # indices from left column
-        print(*original_indices, sep=',')
+        np.random.seed(0)
+        first_index = np.random.choice(n)
+        first_point = observations[first_index]
+        initial_centroid_indices = [first_index]
+        centroids = [first_point]
+        while z < k - 1:
+            probabilities = []
+            distances = []
+            for i, point in enumerate(observations):
+                distance = find_min_distance(centroids, point)
+                distances.append(distance)
+            sum_of_distances = sum(distances)
 
-        spk.c_code_kmeans(initial_centroids, observations.tolist(), k, n, dim)  # C code
+            for i, distance in enumerate(distances):
+                probabilities.append(distance / sum_of_distances) if sum_of_distances != 0 else probabilities.append(0)
+
+            chosen_centroid_index = np.random.choice(n, size=None, p=probabilities)
+            chosen_centroid = observations[chosen_centroid_index]
+            initial_centroid_indices.append(chosen_centroid_index)
+            centroids.append(chosen_centroid)
+
+            z += 1
+
+        print(*initial_centroid_indices, sep=',')
+
+        spk.c_code_kmeans(initial_centroid_indices, observations, k, n, dim)  # C code
+        print("back to python")
 
     elif goal == 'wam':
         spk.c_code(original_k, data_arr, 1, original_n, original_dim)
@@ -41,34 +53,23 @@ def main(original_k: int, goal: str, file_name: str):
     elif goal == 'jacobi':
         spk.c_code(original_k, data_arr, 4, original_n, original_dim)
 
-
-def update_distances(distances, centroids, observations, z, n):
-    for i in range(n):
-        distances[i] = find_min_distance(centroids, observations, i, z)
-    return distances
+    print("passed elif's")
 
 
-def find_min_distance(centroids, observations, obs_index, z):
-    min_distance = np.inf
-    for i in range(z+1):
-        current_distance = calculate_distance(observations[centroids[i]], observations[obs_index])
-        if current_distance<min_distance:
-            min_distance=current_distance
+def find_min_distance(centroids, vector):
+    min_distance = float('inf')
+    for i in range(len(centroids)):
+        current_distance = calc_distance(centroids[i], vector)
+        if min_distance > current_distance:
+            min_distance = current_distance
     return min_distance
 
 
-def calculate_distance(centroid, observation):
-    distance = 0
-    for i in range(len(observation)):
-        distance+= (observation[i]-centroid[i])**2
-    return distance
-
-
-def choose_next_centroid(distances, observations):
-    sum = np.sum(distances)
-    probabilities = [(i/sum) for i in distances]
-    res = np.random.choice(len(observations), p=probabilities)
-    return res
+def calc_distance(point_1, point_2):
+    dis = 0
+    for i in range(len(point_1)):
+        dis += (point_1[i] - point_2[i]) ** 2
+    return dis
 
 
 if __name__ == "__main__":
@@ -77,3 +78,4 @@ if __name__ == "__main__":
         print("Invalid Input!")
         exit(1)
     main(int(sys.argv[1]), sys.argv[2], sys.argv[3])
+    print("passed main")
